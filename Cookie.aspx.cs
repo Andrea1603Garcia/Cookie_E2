1f68a2f32984da8b51ed538d8e2b931630027c04
Andrea1603Garcia <melissagarciaromero57@gmail.com>
2025-03-03 17:13:38 -0600

    Primer Commit

---
+++ Cookie.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp_E2_4955454
+{
+    public partial class Cookie : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+            Response.Redirect
+    (
+"Cookies2.aspx" + "?ddlCategory=" + ddlCategory.SelectedValue + "&ddlSupplier=" + ddlSupplier.SelectedValue + "&txtProducto=" + txtProduct.Text + "&txtDescripcion=" + txtDescripcion.Text + "&txtImagen=" + txtImagen.Text + "&decprice=" + txtPrice.Text + "&byNumberInStock=" + txtNumberInStock.Text + "&byNumberOnOrder=" + txtNumberOnOrder.Text + "&byReorderLevel=" + txtNumberOnOrder.Text);
+
+        }
+    }
+}
